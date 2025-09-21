b0c2974a0b471799221b789bf567121d89e09562
贺若航 <2359952636@qq.com>
2025-09-21 09:12:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # 在此处编写代码
+try:
+    year_input = input("请输入年份：")
+    if year_input.isdigit():
+        year = int(year_input)
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(year, "是闰年")
+        else:
+            print(year, "不是闰年")
+    else:
+        print("输入年份含有非数字")
+except ValueError:
+    print("输入年份错误")
