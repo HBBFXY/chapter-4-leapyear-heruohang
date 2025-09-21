d1c90803fe03d00d1a97a5dbf5e8df3d8aa81efc
贺若航 <2359952636@qq.com>
2025-09-21 09:15:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# 在此处编写代码
+year_input = input("请输入一个年份: ")
 try:
-    year_input = input("请输入年份：")
-    if year_input.isdigit():
-        year = int(year_input)
-        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-            print(year, "是闰年")
-        else:
-            print(year, "不是闰年")
-    else:
-        print("输入年份含有非数字")
+    year = int(year_input)
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print("是闰年")
+    else:
+        print("不是闰年")
 except ValueError:
-    print("输入年份错误")
+    print("输入错误: 请输入有效的年份数字")
